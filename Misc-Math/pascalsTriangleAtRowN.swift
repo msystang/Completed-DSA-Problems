848d3787afa789627e286071f5658baed8524194
Sunni Tang <32780441+msystang@users.noreply.github.com>
2020-04-11 23:54:31 -0400

    Create pascalsTriangleAtRowN.swift

---
+++ Misc-Math/pascalsTriangleAtRowN.swift
@@ -0,0 +1,34 @@
+/*
+Nth Row of Pascal's Triangle
+
+Given a non-negative integer n, write a function that returns the nth rows of Pascal's triangle.  Use a recursive strategy to write this function.
+  
+
+ Input: 5
+ Output:
+ [
+      [1],
+     [1,1],
+    [1,2,1],
+   [1,3,3,1],
+  [1,4,6,4,1] <--
+ ]
+ */
+
+func pascalsTriangle(atRow n: Int) -> [Int] {
+    guard n > 0 else { return [0] }
+    guard n != 1 else { return [1] }
+
+    var arr = [Int]()
+
+    for int in 1...n {
+        if int == 1 || int == n {
+            arr.append(1)
+        } else {
+            arr.append(pascalsTriangle(atRow: n-1)[int-1] + pascalsTriangle(atRow: n-1)[int-2])
+        }
+    }
+    return arr
+}
+
+pascalsTriangle(atRow: 5)
