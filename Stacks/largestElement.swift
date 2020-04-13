92c7d2b51595a673c73a2564549bb4be1af74991
Sunni Tang <32780441+msystang@users.noreply.github.com>
2020-04-13 15:50:46 -0400

    Update largestElement.swift

--- Stacks/largestElement.swift
+++ Stacks/largestElement.swift
@@ -3,26 +3,26 @@
 */
 
 struct Stack<T> {
-    private var arr = Array<T>()
+  private var arr = Array<T>()
     
-    mutating func pop() -> T? {
-        return arr.popLast()
-    }
+  mutating func pop() -> T? {
+    return arr.popLast()
+  }
     
-    mutating func push(element: T) {
-        arr.append(element)
-    }
+  mutating func push(element: T) {
+    arr.append(element)
+  }
     
-    func peek() -> T? {
-        guard arr.count > 0 else {
-            return nil
-        }
-        return arr[arr.count - 1]
+  func peek() -> T? {
+    guard arr.count > 0 else {
+      return nil
     }
+    return arr[arr.count - 1]
+  }
     
-    func isEmpty() -> Bool {
-        return arr.count == 0
-    }
+  func isEmpty() -> Bool {
+    return arr.count == 0
+  }
 }
 
 func largestElement(in stack: Stack<Int>) -> Int {
