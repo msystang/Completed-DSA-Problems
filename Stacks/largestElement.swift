f65803599635ad3ac6c96e98f696932a0b9dd9ee
Sunni Tang <32780441+msystang@users.noreply.github.com>
2020-04-13 15:49:55 -0400

    Create largestElement.swift

---
+++ Stacks/largestElement.swift
@@ -0,0 +1,41 @@
+/*
+  Find the largest integer in a Stack of Ints
+*/
+
+struct Stack<T> {
+    private var arr = Array<T>()
+    
+    mutating func pop() -> T? {
+        return arr.popLast()
+    }
+    
+    mutating func push(element: T) {
+        arr.append(element)
+    }
+    
+    func peek() -> T? {
+        guard arr.count > 0 else {
+            return nil
+        }
+        return arr[arr.count - 1]
+    }
+    
+    func isEmpty() -> Bool {
+        return arr.count == 0
+    }
+}
+
+func largestElement(in stack: Stack<Int>) -> Int {
+  var stack = stack
+  var largestValue: Int = 0
+    
+  while !stack.isEmpty() {
+    guard let currentValue = stack.pop() else { return largestValue }
+  
+    if currentValue > largestValue {
+      largestValue = currentValue
+    }
+  }
+  
+  return largestValue
+}
