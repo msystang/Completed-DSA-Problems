8b91da4016aeeffacdad9143c7c5f1dc043f5c18
Sunni Tang <32780441+msystang@users.noreply.github.com>
2020-04-21 15:17:29 -0400

    Create allPathsSourceTarget.swift

---
+++ Graphs/allPathsSourceTarget.swift
@@ -0,0 +1,52 @@
+/*
+ All Paths From Source to Target
+
+ Given a directed, acyclic graph of N nodes.  Find all possible paths from node 0 to node N-1, and return them in any order.
+
+ The graph is given as follows:  the nodes are 0, 1, ..., graph.length - 1.  graph[i] is a list of all nodes j for which the edge (i, j) exists.
+
+ 
+ Example:
+ Input: [[1,2], [3], [3], []]
+ Output: [[0,1,3],[0,2,3]]
+ Explanation: The graph looks like this:
+ 0--->1
+ |    |
+ v    v
+ 2--->3
+ There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+ Note:
+
+ The number of nodes in the graph will be in the range [2, 15].
+ You can print different paths in any order, but you should keep the order of nodes inside one path.
+ */
+
+
+func allPathsSourceTarget(_ graph: [[Int]]) -> [[Int]] {
+    var result = [[Int]]()
+    let path = [0]
+    
+    depthFirstSearch(graph, 0, path, &result)
+    
+    return result
+}
+
+private func depthFirstSearch(_ graph: [[Int]],
+                 _ index: Int,
+                 _ path: [Int],
+                 _ result: inout [[Int]]) {
+    
+    if index == graph.count - 1 {
+        result.append(path)
+        return
+    }
+    
+    for node in graph[index] {
+        var path = path
+        path.append(node)
+        depthFirstSearch(graph, node, path, &result)
+    }
+}
+
+let input = [[1,2], [3], [3], []]
+print(allPathsSourceTarget(input))
