e821ddf5970b30f205437197d36a7baaa53622a0
Sunni Tang <32780441+msystang@users.noreply.github.com>
2020-07-14 15:49:55 -0400

    Create maxDepth.swift

---
+++ Trees/maxDepth.swift
@@ -0,0 +1,46 @@
+/*
+Maximum Depth of Binary Tree - https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+ 
+func maxDepth(_ root: TreeNode?) -> Int {
+  guard let root = root else { return 0 }
+    
+  let left = maxDepth(root.left)
+  let right = maxDepth(root.right)
+        
+  return 1 + max(left,right)
+}
